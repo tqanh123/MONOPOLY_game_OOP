d8622a73e1ade09b0ea580b95a359753cb7a4237
TVDatIT2003 <tvdat1470@gmail.com>
2023-05-15 15:00:14 +0700

    Add
    đâsd
    
    Co-Authored-By: tqanh123 <52412090+tqanh123@users.noreply.github.com>

---
+++ Demo/Demo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author acer
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        EnterNumberOfPlayers enter = new EnterNumberOfPlayers();
+        enter.show();
+    }
+}
