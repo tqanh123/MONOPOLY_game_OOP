e6d70025245c95fd3bc211bfa788b3e538085c23
TVDatIT2003 <tvdat1470@gmail.com>
2023-05-15 17:37:15 +0700

    Add

--- Demo/TwoPlayers.java
+++ Demo/TwoPlayers.java
@@ -110,7 +110,7 @@ public class TwoPlayers extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        Main main = new Main();
+        //Main main = new Main();
         
 
     }//GEN-LAST:event_jButton1ActionPerformed
