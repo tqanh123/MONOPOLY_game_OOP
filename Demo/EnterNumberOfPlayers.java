8401443ccc8762c0bd33adec2c4b77adbbbcce10
TVDatIT2003 <tvdat1470@gmail.com>
2023-05-16 10:54:01 +0700

    add

--- Demo/EnterNumberOfPlayers.java
+++ Demo/EnterNumberOfPlayers.java
@@ -3,15 +3,7 @@ import java.awt.Toolkit;
 import java.awt.event.WindowEvent;
 import javax.swing.JOptionPane;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 
-/**
- *
- * @author acer
- */
 public class EnterNumberOfPlayers extends javax.swing.JFrame {
 
     /**
