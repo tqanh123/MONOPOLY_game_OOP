7a9e51d911f0a8f23b202394dec35bbd4e24d75f
Khanh Nguyen <113000662+biubiu2408@users.noreply.github.com>
2023-05-20 22:12:04 +0700

    Add files via upload

--- Menu/Main1.java
+++ Menu/Main1.java
@@ -1,8 +1,10 @@
 package Menu;
 
-public class Main1 {
-    public static void main(String[] args) {
-        LaunchPage launchPage = new LaunchPage();
+import java.io.IOException;
 
+public class Main1 {
+    public static void main(String[] args) throws IOException {
+        LaunchPage launchPage = new LaunchPage(400, 400, "5\\res\\MenuBackground\\monopoly_background.png");
+        launchPage.run();
     }
 }
