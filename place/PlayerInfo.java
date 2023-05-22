d391878e28c444fbc8cd5805c664f3f85f6046a6
TVDatIT2003 <tvdat1470@gmail.com>
2023-05-23 00:11:07 +0700

    Update

--- place/PlayerInfo.java
+++ place/PlayerInfo.java
@@ -68,42 +68,77 @@ Player money = new Player(gp, null, 0, 0, 0, 0);
         p1.setColor(Color.MAGENTA);
         p1.drawString("Player 1: " + money.getMoney(),gp.boardsize , gp.Menuheight*16+30);
         p1.drawImage(avatar[0].image, gp.boardsize, gp.Menuheight*16+40, 65, 65, null);
+
+        p1.setFont(font);
+        p1.setColor(Color.BLUE);
+        p1.fillRect(gp.boardsize,gp.Menuheight*16+ (832- gp.Menuheight*16)/4, gp.infoPlayer*16,(832- gp.Menuheight*16)/4);
+        p1.setColor(Color.CYAN);
+        p1.drawString("Player 2: "+ money.getMoney(),gp.boardsize ,(gp.Menuheight*16+ ((832- gp.Menuheight*16)/4))+30);
+        p1.drawImage(avatar[1].image, gp.boardsize, gp.Menuheight*16+235, 65, 65, null);
+
+        p1.setFont(font);
+        p1.setColor(Color.YELLOW);
+        p1.fillRect(gp.boardsize,gp.Menuheight*16+ ((832- gp.Menuheight*16)/4)*2, gp.infoPlayer*16,(832- gp.Menuheight*16)/4);
+        p1.setColor(Color.MAGENTA);
+        p1.drawString("Player 3: "+ money.getMoney(),gp.boardsize ,(gp.Menuheight*16 + ((832- gp.Menuheight*16)/4)*2)+30);
+        p1.drawImage(avatar[2].image, gp.boardsize, gp.Menuheight*16+430, 65, 65, null);
+
+        p1.setFont(font);
+        p1.setColor(Color.RED);
+        p1.fillRect(gp.boardsize,gp.Menuheight*16+ ((832- gp.Menuheight*16)/4)*3, gp.infoPlayer*16,(832- gp.Menuheight*16)/4);
+        p1.setColor(Color.CYAN);
+        p1.drawString("Player 4: " + money.getMoney(),gp.boardsize ,(gp.Menuheight*16 + ((832- gp.Menuheight*16)/4)*3)+30);
+        p1.drawImage(avatar[3].image, gp.boardsize, gp.Menuheight*16+630, 65, 65, null);
     }
     
     public void draw2(Graphics2D p2){
         p2.setFont(font);
+        p2.setColor(Color.GREEN);
+        p2.fillRect(gp.boardsize, gp.Menuheight*16, gp.infoPlayer*16,(832- gp.Menuheight*16)/3);
+        p2.setColor(Color.MAGENTA);
+        p2.drawString("Player 1: " + money.getMoney(),gp.boardsize , gp.Menuheight*16+30);
+        p2.drawImage(avatar[0].image, gp.boardsize, gp.Menuheight*16+40, 65, 65, null);
+
+        p2.setFont(font);
         p2.setColor(Color.BLUE);
-        p2.fillRect(gp.boardsize,gp.Menuheight*16+ (832- gp.Menuheight*16)/4, gp.infoPlayer*16,(832- gp.Menuheight*16)/4);
+        p2.fillRect(gp.boardsize,gp.Menuheight*16+ (832- gp.Menuheight*16)/3, gp.infoPlayer*16,(832- gp.Menuheight*16)/3);
         p2.setColor(Color.CYAN);
-        p2.drawString("Player 2: "+ money.getMoney(),gp.boardsize ,(gp.Menuheight*16+ ((832- gp.Menuheight*16)/4))+30);
-        p2.drawImage(avatar[1].image, gp.boardsize, gp.Menuheight*16+235, 65, 65, null);
+        p2.drawString("Player 2: "+ money.getMoney(),gp.boardsize ,(gp.Menuheight*16+ ((832- gp.Menuheight*16)/3))+30);
+        p2.drawImage(avatar[1].image, gp.boardsize, gp.Menuheight*16+295, 80, 80, null);
+
+        p2.setFont(font);
+        p2.setColor(Color.YELLOW);
+        p2.fillRect(gp.boardsize,gp.Menuheight*16+ ((832- gp.Menuheight*16)/3)*2, gp.infoPlayer*16,(832- gp.Menuheight*16)/3);
+        p2.setColor(Color.MAGENTA);
+        p2.drawString("Player 3: "+ money.getMoney(),gp.boardsize ,(gp.Menuheight*16 + ((832- gp.Menuheight*16)/3)*2)+30);
+        p2.drawImage(avatar[2].image, gp.boardsize, gp.Menuheight*16+560, 65, 65, null);
     }
     
     public void draw3(Graphics2D p3){
         p3.setFont(font);
-        p3.setColor(Color.YELLOW);
-        p3.fillRect(gp.boardsize,gp.Menuheight*16+ ((832- gp.Menuheight*16)/4)*2, gp.infoPlayer*16,(832- gp.Menuheight*16)/4);
+        p3.setColor(Color.GREEN);
+        p3.fillRect(gp.boardsize, gp.Menuheight*16, gp.infoPlayer*16,(832- gp.Menuheight*16)/2);
         p3.setColor(Color.MAGENTA);
-        p3.drawString("Player 3: "+ money.getMoney(),gp.boardsize ,(gp.Menuheight*16 + ((832- gp.Menuheight*16)/4)*2)+30);
-        p3.drawImage(avatar[2].image, gp.boardsize, gp.Menuheight*16+430, 65, 65, null);
-    }
+        p3.drawString("Player 1: " + money.getMoney(),gp.boardsize , gp.Menuheight*16+30);
+        p3.drawImage(avatar[0].image, gp.boardsize, gp.Menuheight*16+40, 65, 65, null);
 
-    public void draw4(Graphics2D p4){
-        p4.setFont(font);
-        p4.setColor(Color.RED);
-        p4.fillRect(gp.boardsize,gp.Menuheight*16+ ((832- gp.Menuheight*16)/4)*3, gp.infoPlayer*16,(832- gp.Menuheight*16)/4);
-        p4.setColor(Color.CYAN);
-        p4.drawString("Player 4: " + money.getMoney(),gp.boardsize ,(gp.Menuheight*16 + ((832- gp.Menuheight*16)/4)*3)+30);
-        p4.drawImage(avatar[3].image, gp.boardsize, gp.Menuheight*16+630, 65, 65, null);
-    }
+        p3.setFont(font);
+        p3.setColor(Color.BLUE);
+        p3.fillRect(gp.boardsize,gp.Menuheight*16+ (832- gp.Menuheight*16)/2, gp.infoPlayer*16,(832- gp.Menuheight*16)/2);
+        p3.setColor(Color.CYAN);
+        p3.drawString("Player 2: "+ money.getMoney(),gp.boardsize ,(gp.Menuheight*16+ ((832- gp.Menuheight*16)/2))+30);
+        p3.drawImage(avatar[1].image, gp.boardsize, gp.Menuheight*16+425, 80, 80, null);
 
-    
+    }
 
     public void draw(Graphics2D g2) {
-        draw1(g2);
-        draw2(g2);
-        draw3(g2);
-        draw4(g2);
-          
+        EnterNumberOfPlayers enter = new EnterNumberOfPlayers();
+        if(enter.getNum() == 4){
+            draw1(g2);
+        } else if(enter.getNum() == 3){
+            draw2(g2);
+        } else {
+            draw3(g2);
+        }      
     }
 }
